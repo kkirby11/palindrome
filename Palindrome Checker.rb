784d2fa368baaa032fafb8658cf9ec8873b6cf06
kristina kirby <kristinakirby@mac.com>
2018-05-18 10:32:58 -0600

    another commit

---
+++ Palindrome Checker.rb
@@ -0,0 +1,40 @@
+##ask user for word to compare
+##take in word and convert to a intiger
+##every letter of the alphabet is assigned an number
+##each letter in user input word is cased to letter map
+## not sure if i can use js to compare the placement of letters loop or like vowel count shift
+
+
+class Word
+    def user_input(word)
+        @user_input = word
+    end
+
+    def user_input
+        @user_input
+    end
+end
+
+    word = word.new
+    word.user_input
+
+def create_compare
+    puts "enter the word you would like checked"
+    firstword = gets.to_strip.downcase
+    
+def palindrome
+    ##    
+    palindrome = gets. ##not correct come back to..Important!
+
+case palindrome 
+    when
+
+    if
+
+    else
+
+
+create_compare
+    end
+
+### Clayton reversed the word and compared it. first he broke it up and then he compared and reversed.
